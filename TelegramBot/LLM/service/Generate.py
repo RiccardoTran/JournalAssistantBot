92f24b578094ffdbb7b84b4b11aa6661a8b2ac37
MACBOOK <macbook@MacBook-Pro-di-MACBOOK.local>
2025-03-22 13:49:35 +0100

    edit Generate#generateResponse argument to list[str]

--- TelegramBot/LLM/service/Generate.py
+++ TelegramBot/LLM/service/Generate.py
@@ -1,12 +1,17 @@
 from TelegramBot.configuration.LLMApiConfig import GROQ_API_KEY, LLM_PROVIDER_API_URL
 import openai
 
+from typing import List
+
+
 client = openai.OpenAI(
     base_url=LLM_PROVIDER_API_URL,
     api_key=GROQ_API_KEY  
 )
 
-async def generate_example_response(prompt) -> str:
+async def generateResponse(strings: List[str]) -> str:
+    prompt = " ".join(strings)
+
     response = client.chat.completions.create(
         model="llama-3.2-90b-vision-preview",
         messages=[{"role": "user", "content": prompt}],
